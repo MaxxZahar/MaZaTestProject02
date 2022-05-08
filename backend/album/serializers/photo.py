328dfbd59cb1be4da62f6226dc78d6a887aa7130
Maksim Zakharenkov <maxxzahar@rambler.ru>
2022-05-08 20:12:45 +0300

    image size validator

--- backend/album/serializers/photo.py
+++ backend/album/serializers/photo.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from ..models import Photo, Album, Tag
-from ..settings import ALLOWED_IMAGE_EXTENSIONS
+from ..settings import ALLOWED_IMAGE_EXTENSIONS, FILE_SIZE_LIMIT
 from django.core.validators import FileExtensionValidator
 from .tag import TagSerializer
 
@@ -10,11 +10,20 @@ class UserPhotoForeignKey(serializers.PrimaryKeyRelatedField):
         return Album.objects.filter(user=self.context.get('request').user)
 
 
+def validate_file_size(temp_file):
+    if temp_file.size > FILE_SIZE_LIMIT * 10 ** 6:
+        raise serializers.ValidationError('This image is too large.')
+    return temp_file
+
+
 class PhotoSerializer(serializers.ModelSerializer):
-    img = serializers.ImageField(validators=[FileExtensionValidator(ALLOWED_IMAGE_EXTENSIONS)])
+    img = serializers.ImageField(validators=[FileExtensionValidator(ALLOWED_IMAGE_EXTENSIONS), validate_file_size])
     album = UserPhotoForeignKey()
-    tags = TagSerializer(many=True)
+    tags = TagSerializer(many=True, required=False)
 
     class Meta:
         model = Photo
         fields = '__all__'
+
+
+
